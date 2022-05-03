17809e3391e87a223df4304165f8c79d5979d423
edmirgalvan01 <edmirgalvaz@gmail.com>
2022-05-03 13:52:02 -0500

    Se creo la funcion para traer las peliculas en tendencia

--- src/main.js
+++ src/main.js
@@ -1,1 +1,34 @@
+async function getTrendingMoviesPreview() {
+   //We make a request to get the trending movies
+   const response = await fetch(
+      'https://api.themoviedb.org/3/trending/movie/day?api_key=' + API_KEY
+   );
+   const data = await response.json();
 
+   //We save de results in this movies
+   const movies = data.results;
+
+   movies.forEach((movie) => {
+      //This is the container
+      const trandingPreviewMoviesContainer = document.querySelector(
+         '#trendingPreview .trendingPreview-movieList'
+      );
+
+      const movieContainer = document.createElement('div');
+      movieContainer.classList.add('movie-container');
+
+      const movieImg = document.createElement('img');
+      movieImg.classList.add('movie-img');
+      movieImg.setAttribute('alt', movie.title);
+      movieImg.setAttribute(
+         'src',
+         'https://image.tmdb.org/t/p/w300' + movie.poster_path
+      );
+
+      //We add the elements created
+      movieContainer.appendChild(movieImg);
+      trandingPreviewMoviesContainer.appendChild(movieContainer);
+   });
+}
+
+getTrendingMoviesPreview();
